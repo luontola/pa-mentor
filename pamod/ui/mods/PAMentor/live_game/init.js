19be20bac0af68104d9a3436ed9a6b84b4a11797
Esko Luontola <esko.luontola@gmail.com>
2013-12-30 19:52:24 +0200

    Placeholder for the UI elements

---
+++ pamod/ui/mods/PAMentor/live_game/init.js
@@ -0,0 +1,15 @@
+// Copyright © 2013 Esko Luontola <www.orfjackal.net>
+// This software is released under the Apache License 2.0.
+// The license text is at http://www.apache.org/licenses/LICENSE-2.0
+
+$(function () {
+
+    function loadTemplate(element, url) {
+        element.load(url, function () {
+            ko.applyBindings(model, element.get(0));
+        });
+    }
+
+    createFloatingFrame('pa_mentor_frame', 100, 100, {'offset': 'leftCenter', 'left': 0});
+    loadTemplate($('#pa_mentor_frame_content'), 'coui://ui/mods/PAMentor/live_game/pa_mentor.html');
+});

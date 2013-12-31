74979c077956ab77a80349c8e8215b1da39a1557
Esko Luontola <esko.luontola@gmail.com>
2013-12-31 23:09:23 +0200

    Experiment using Q

--- server/experiment.js
+++ server/experiment.js
@@ -2,6 +2,7 @@
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
+var Q = require('q');
 var db = require('./db');
 var rest = require('./rest');
 var games = require('./games');
@@ -9,17 +10,18 @@ var analytics = require('./analytics');
 
 var gameId = process.argv[2] || 11919;
 
-rest.getObject('http://www.nanodesu.info/pastats/report/get?gameId=' + gameId, function (err, game) {
-    if (err) {
-        throw err;
-    }
-    games.save(game, function (err) {
-        console.info(game, err);
-
-        console.info("\nCalculating statistics...");
-        analytics.refreshAndGet(5000 * 100, function (err, stats) {
+Q.nfcall(rest.getObject, 'http://www.nanodesu.info/pastats/report/get?gameId=' + gameId)
+        .then(function (game) {
+            return Q.nfcall(games.save, game);
+        })
+        .then(function () {
+            console.info("\nCalculating statistics...");
+            return Q.nfcall(analytics.refreshAndGet, 5000 * 100);
+        })
+        .then(function (stats) {
             console.info(JSON.stringify(stats, null, 2));
+        })
+        .fin(function () {
             db.close();
-        });
-    });
-});
+        })
+        .done();

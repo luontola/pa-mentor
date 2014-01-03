075991d5cfcaffddd23b18b2d726d4d4b0968cea
Esko Luontola <esko.luontola@gmail.com>
2014-01-03 18:43:16 +0200

    Sampling period parameter

--- server/config.js
+++ server/config.js
@@ -2,10 +2,16 @@
 // This software is released under the Apache License 2.0.
 // The license text is at http://www.apache.org/licenses/LICENSE-2.0
 
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
+const HOUR = 60 * MINUTE;
+const DAY = 24 * HOUR;
+
 var config = {
     port: process.env.PA_MENTOR_PORT || 8080,
     dbUri: process.env.PA_MENTOR_DB_URI || 'mongodb://localhost:27017/paMentorTest',
-    updateInterval: Math.max(1000, process.env.PA_MENTOR_UPDATE_INTERVAL || 60 * 60 * 1000)
+    updateInterval: Math.max(SECOND, process.env.PA_MENTOR_UPDATE_INTERVAL || HOUR),
+    samplingPeriod: Math.max(0, process.env.PA_MENTOR_SAMPLING_PERIOD || 30 * DAY)
 };
 
 module.exports = config;

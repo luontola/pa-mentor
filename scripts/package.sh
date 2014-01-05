313edcb03da710c5a6a4345e39fa37e8a81683e8
Esko Luontola <esko.luontola@gmail.com>
2014-01-05 21:57:18 +0200

    Packaging doesn't anymore need string replacement for configuration

--- scripts/package.sh
+++ scripts/package.sh
@@ -3,15 +3,11 @@ set -eu
 : ${1:? Usage: $0 VERSION}
 VERSION="$1"
 FILENAME="PAMentor_v$VERSION.zip"
-DEV_SERVER="http://127\.0\.0\.1:8080"
-PROD_SERVER="http://pa-mentor.orfjackal.net"
 set -x
 
 rm -rf dist/PAMentor
 mkdir -p dist
 cp -r pamod dist/PAMentor
-
-find dist/PAMentor -type f | xargs sed -i -e "s|$DEV_SERVER|$PROD_SERVER|g"
 
 cd dist
 rm -f "$FILENAME"
